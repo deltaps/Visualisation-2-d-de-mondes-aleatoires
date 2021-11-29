f4cad01ad042775f30744e105e482ca795bbf3af
Sacha Pronost <21901956@etu.unicaen.fr>
2021-11-29 15:51:29 +0100

    Ajout class vide

---
+++ src/model/Case.java
@@ -0,0 +1,9 @@
+package model;
+
+public class Case {
+    // Représentation d'une case
+    private int x;
+    private int y;
+    private int elevation;
+
+}
---
+++ src/model/WorldMap.java
@@ -0,0 +1,8 @@
+package model;
+
+public class WorldMap {
+    // Représentation de notre monde
+    // Liste de Case
+    // Génération aléatoire de monde
+    private Case[][] worldMap;
+}
