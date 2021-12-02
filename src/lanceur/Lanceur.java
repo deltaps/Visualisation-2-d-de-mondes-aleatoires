235d23fc2759d8688719bfff10d41f6a23cec716
Sacha Pronost <21901956@etu.unicaen.fr>
2021-12-02 17:00:14 +0100

    Clean

--- src/lanceur/Lanceur.java
+++ src/lanceur/Lanceur.java
@@ -4,9 +4,6 @@ import controlleur.Controller;
 
 public class Lanceur {
     public static void main(String[] args){
-        AffichageTerminal affichageTerminal = new AffichageTerminal();
-        affichageTerminal.lancement();
-        
         new Controller();
     }
 }
