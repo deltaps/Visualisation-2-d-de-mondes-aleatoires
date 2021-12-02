cb38b7918c9364f66e12f263ef87eb92939bde7c
Mathieu Vallee <21910887@etu.unicaen.fr>
2021-12-02 16:33:53 +0100

    Changes by sacha

--- src/model/PredefinedWorldMap.java
+++ src/model/PredefinedWorldMap.java
@@ -5,12 +5,12 @@ public class PredefinedWorldMap implements MapStrategy {
     @Override
     public Case[][] creationWorldMap() {
         // Roughness <= 128/width+height
-        Noise noise = new Noise(null,128.0f,20,20);
+        Noise noise = new Noise(null,128.0f,50,50);
         noise.initialise();
         float[][] grid = noise.getGrid_();
-        Case[][] worldmap = new Case[20][20];
-        for(int i = 0; i < 20; i++){
-            for(int j = 0; j < 20; j++){
+        Case[][] worldmap = new Case[50][50];
+        for(int i = 0; i < 50; i++){
+            for(int j = 0; j < 50; j++){
                 worldmap[i][j] = new Case(i,j, (int) (grid[i][j]));
             }
         }
