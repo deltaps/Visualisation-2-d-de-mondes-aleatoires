1a2e312eee5f701879f76fa925d54af0584b555d
Sacha Pronost <21901956@etu.unicaen.fr>
2022-02-15 16:36:38 +0100

    Meilleure performance (affichage) + drawLine au lieu de fillRect

--- src/vue/UserCamera.java
+++ src/vue/UserCamera.java
@@ -1,5 +1,6 @@
 package vue;
 
+import model.Case;
 import model.ColorMap;
 import model.WorldMap;
 
@@ -29,13 +30,22 @@ public class UserCamera extends JPanel implements CameraStrategy {
     public UserCamera(WorldMap map, ColorMap colorMap) {
         this.x = 10;
         this.y = 10;
-        this.distance = 40;
+        this.distance = 50;
 
         this.map = map;
         this.colorMap = colorMap;
-        this.height = this.map.getCase(this.x,this.y).getElevation();
+        //this.height = this.map.getCase(this.x,this.y).getElevation();
+        int max = 0;
+        for(Case[] cases : map.getWorldMap()) {
+            for (Case casess : cases) {
+                if (casess.getElevation() > max) {
+                    max = casess.getElevation();
+                }
+            }
+        }
+        this.height = max;
         this.scaleHeight = 6.666666f;
-        this.horizon = 120;
+        this.horizon = 60;
         this.phi = 0;
 
         this.screenWidth = 600;
@@ -49,32 +59,76 @@ public class UserCamera extends JPanel implements CameraStrategy {
         super.paintComponent(g);
         float sinphi = (float)Math.sin(this.phi);
         float cosphi = (float)Math.cos(this.phi);
-        for(int distanceActu = this.distance; distanceActu != 1; distanceActu--){
 
-            Point pleft = new Point((-cosphi*distanceActu - sinphi*distanceActu) + this.x, (sinphi*distanceActu - cosphi*distanceActu) + this.y); // Point le plus a gauche
-            Point pright = new Point((cosphi*distanceActu - sinphi*distanceActu) + this.x,(-sinphi*distanceActu - cosphi*distanceActu) + this.y);
+        double[] ybuffer = new double[screenWidth];// Tableau de taille screenHeight contenant que des 0
+
+        for(int i = 0; i < screenWidth; i++){
+            ybuffer[i] = screenHeight;
+        }
+
+        double dz = 1.0;
+        double z = 1.0;
+
+        while(z < this.distance){
+            Point pleft = new Point((float) ((-cosphi*z - sinphi*z) + this.x), (float) ((sinphi*z - cosphi*z) + this.y)); // Point le plus a gauche
+            Point pright = new Point((float) ((cosphi*z - sinphi*z) + this.x), (float) ((-sinphi*z - cosphi*z) + this.y));
 
             float dx = (pright.getX() - pleft.getX()) / this.screenWidth; // Ratio du nombre de point
             float dy = (pright.getY() - pleft.getY()) / this.screenWidth;
-            for(int i = 0; i < this.screenWidth; i++){ // on boucle sur la taille de l'écran
-                float heightOnScreen = (this.height - this.map.getCase(Math.round(pleft.getX()),Math.round(pleft.getY())).getElevation()) / distanceActu * this.scaleHeight + this.horizon;
+
+            for(int i = 0; i < this.screenWidth; i++){
+                double heightOnScreen = (this.height - this.map.getCase(Math.round(pleft.getX()),Math.round(pleft.getY())).getElevation()) / z * this.scaleHeight + this.horizon;
                 int[] RGB =  new int[]{this.colorMap.getCase(Math.round(pleft.getX()), Math.round(pleft.getY()))[0], this.colorMap.getCase(Math.round(pleft.getX()), Math.round(pleft.getY()))[1], this.colorMap.getCase(Math.round(pleft.getX()), Math.round(pleft.getY()))[2]};
-                drawVerticalLine(g, (int) (255-heightOnScreen), i, new Color(RGB[0],RGB[1],RGB[2]));
+                //drawVerticalLine(g, (int) (255-heightOnScreen), i, new Color(RGB[0],RGB[1],RGB[2]));
+                drawVerticalLine(g,(int) (heightOnScreen),ybuffer[i],i,new Color(RGB[0],RGB[1],RGB[2]));
+                if(heightOnScreen < ybuffer[i]){
+                    ybuffer[i] = heightOnScreen;
+                }
+                pleft.setX(pleft.getX() + dx); // Problème a régler par rapport au débordement
+                pleft.setY(pleft.getY() + dy);
+            }
+            z += dz;
+            dz += 0.2;
+        }
+
+        /*
+        for(int distanceActu = this.distance; distanceActu != 1; distanceActu--) {
+
+            Point pleft = new Point((-cosphi * distanceActu - sinphi * distanceActu) + this.x, (sinphi * distanceActu - cosphi * distanceActu) + this.y); // Point le plus a gauche
+            Point pright = new Point((cosphi * distanceActu - sinphi * distanceActu) + this.x, (-sinphi * distanceActu - cosphi * distanceActu) + this.y);
+
+            float dx = (pright.getX() - pleft.getX()) / this.screenWidth; // Ratio du nombre de point
+            float dy = (pright.getY() - pleft.getY()) / this.screenWidth;
+            for (int i = 0; i < this.screenWidth; i++) { // on boucle sur la taille de l'écran
+                float heightOnScreen = (this.height - this.map.getCase(Math.round(pleft.getX()), Math.round(pleft.getY())).getElevation()) / distanceActu * this.scaleHeight + this.horizon;
+                int[] RGB = new int[]{this.colorMap.getCase(Math.round(pleft.getX()), Math.round(pleft.getY()))[0], this.colorMap.getCase(Math.round(pleft.getX()), Math.round(pleft.getY()))[1], this.colorMap.getCase(Math.round(pleft.getX()), Math.round(pleft.getY()))[2]};
+                drawVerticalLine(g, (int) (heightOnScreen),screenHeight, i, new Color(RGB[0], RGB[1], RGB[2]));
                 pleft.setX(pleft.getX() + dx); // Problème a régler par rapport au débordement
                 pleft.setY(pleft.getY() + dy);
             }
         }
+
+
+         */
+
     }
 
-    public void drawVerticalLine(Graphics g, int height, int x, Color color) {
+    public void drawVerticalLine(Graphics g, double heightBottom,double heightTop, int x, Color color) {
+        if(heightTop <= heightBottom) return;
+        if(heightTop < 0){
+            heightTop = 0;
+        }
         g.setColor(color);
-        g.fillRect(x, this.screenHeight - height, 1, height); //a voir avec drawline
+        g.drawLine(x, (int) Math.floor(heightTop),x, (int) Math.floor(heightBottom));
+        //g.fillRect(x, this.screenHeight - height, 1, height); //a voir avec drawline
     }
 
-    // Fonction pour bouger l'utilisateur en fonction de Keyboard (l'écouteur du clavier)
+    // Fonction pour bouger l'utilisateur en fonction de Keyboard (l'écouteur du clavier) et de l'angle de la camera
     public void moveUser(int direction) {
         int angle = (int) Math.round(this.phi * (180/Math.PI));
         boolean diagonal = false;
+        //Dans la suite on vérifie l'angle de la camera, ainsi en fonction de l'angle et de la direction que l'utilisateur à choisie,
+        //on change la position de la camera.
         if(angle >= 30 && angle < 60){
             diagonal = true;
         }
@@ -139,7 +193,7 @@ public class UserCamera extends JPanel implements CameraStrategy {
                 break;
         }
 
-        this.height = this.map.getCase(this.x,this.y).getElevation();
+        //this.height = this.map.getCase(this.x,this.y).getElevation();
 
         repaint();
     }
